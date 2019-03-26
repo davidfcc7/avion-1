1ff2e198c945931ea2044e209f60fab7bde912ab
Ivan Calderon <ivancho_1096@hotmail.com>
2019-03-25 22:52:21 -0500

    Main, Principal

---
+++ src/main/java/com/mycompany/aviones/Main.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.aviones;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+/**
+ *
+ * @author Ivan Calderon, David Caicedo 
+ * @version 1.0
+ */
+public class Main {
+     public static void main(String[] args) throws IOException, ParseException {
+     
+        
+        Principal inicio =  new Principal();  
+        inicio.Menu();
+}
+}
